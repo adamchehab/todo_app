2413609d1f983dc9ad6a3665122692195cb1c0be
Adam Shehab <a.chehab@keep-calm.ru>
2023-10-23 01:45:19 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,30 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-// import tasks_data from "./tasks.json";
 
-import { Header, NavBlock, TaskList, AddTaskPopup } from "./components/index";
-
-import { db } from "./firebase";
-import { query, collection, onSnapshot } from "firebase/firestore";
+function MyComponent({ title, count }) {
+  return (
+    <>
+      <div className="card">
+        <div className="content">
+          <div className="title">
+            <div className="text">
+              {title}
+            </div>
+            <div className="count">
+              {count}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default function TaskListApp() {
-	const [tasks, setTasks] = useState([]);
-	const [filter, setFilter] = useState(false);
-	const [searchItem, setSearchItem] = useState("");
-	const [addTaskWindow, setAddTaskWindow] = useState(false);
-
-	// Load data from firebase
-	useEffect(() => {
-		const q = query(collection(db, "todos"));
-		const unsub = onSnapshot(q, (querySnapshot) => {
-			const todosArr = [];
-			querySnapshot.forEach((doc) => {
-				todosArr.push({ ...doc.data(), id: doc.id });
-			});
-			setTasks(todosArr);
-		});
-		return () => unsub();
-	}, []);
-
-	// Filter
-	let filter_tasks = tasks;
-	if (filter === true) {
-		filter_tasks = tasks.filter((task) => task.completed === false);
-	}
-	filter_tasks = filter_tasks.filter(
-		(item) =>
-			item.name.toLowerCase().includes(searchItem.toLowerCase()) ||
-			item.project.toLowerCase().includes(searchItem.toLowerCase())
-	);
-
-	return (
-		<>
-			{/* Header */}
-			<Header />
-			{/* Page Content div*/}
-			<div className="bg-darkColor justify-center items-center h-screen">
-				{/* Nav Block */}
-				<NavBlock
-					searchItem={searchItem}
-					setSearchItem={setSearchItem}
-					filter={filter}
-					setFilter={setFilter}
-					setAddTaskWindow={setAddTaskWindow}
-				/>
-				{/* Tasks List */}
-				<TaskList tasks={filter_tasks} setTasks={setTasks} />
-				{/* Add task popup */}
-				<AddTaskPopup
-					addTaskWindow={addTaskWindow}
-					setAddTaskWindow={setAddTaskWindow}
-					tasks={tasks}
-					setTasks={setTasks}
-				/>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <MyComponent title="Товар 1" count={1} />
+      <MyComponent title="Товар второй" count={3} />
+      <MyComponent title="Долгое оп. товара" count={3} />
+    </>
+  );
 }
